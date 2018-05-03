07becc35b1c01c6a42d7d15818789deb94bf77db
wanghaitao <wanghaitao@i.shu.edu.cn>
2018-05-03 12:42:48 +0800

    提取数字特征

--- DataProcessing_2.py
+++ DataProcessing_2.py
@@ -2,65 +2,58 @@ import numpy as np
 import pandas as pd
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
-"""
+
 # 对训练集和测试集的y按照vid排序,和X一一对应
 y_train = pd.read_csv('./dataset/round1_train.csv', encoding='utf-8',
                       low_memory=False, index_col='vid')
 y_test = pd.read_csv('./dataset/round1_test.csv', encoding='utf-8',
                      low_memory=False, index_col='vid')
 y_train, y_test = y_train.sort_index(), y_test.sort_index()
+
+y_train, y_test = y_train.fillna(0), y_test.fillna(0)
+y_train, y_test = y_train.convert_objects(
+    convert_numeric=True), y_test.convert_objects(convert_numeric=True)
 y_train, y_test = y_train.fillna(0), y_test.fillna(0)
 y_train, y_test = y_train.convert_objects(
     convert_numeric=True), y_test.convert_objects(convert_numeric=True)
 
+print(np.isnan(y_train).any())
 print(y_train.info(), y_test.info())
 y_train.to_csv("./dataset/y_train.csv", encoding='utf-8')
 y_test.to_csv("./dataset/y_test.csv", encoding='utf-8')
 print("OKAY!!")
 
-"""
+
 print("Start split train and test dataset!!!")
 # 读取训练集和测试集
 X_train = pd.read_csv('./dataset/train.csv', encoding='utf-8',
                       low_memory=False, header='infer', index_col='vid')
-col_tmp=X_train.describe().columns
+X_test = pd.read_csv('./dataset/test.csv', encoding='utf-8',
+                     low_memory=False, header='infer', index_col='vid')
+col_train_num, col_test_num = X_train.describe().columns, X_test.describe().columns
+X_train,X_test=X_train[col_train_num],X_test[col_test_num]
 
-X_train=X_train[col_tmp]
+print("Start filter features!!!")
+# 筛选缺失率小于0.1的特征
+col_train, col_test = [], []
+for item in X_train.columns:
+    tmp = np.sum(X_train[item].isnull()) / len(X_train)
+    if tmp < 0.95:
+        col_train.append(item)
+for item in X_test.columns:
+    tmp = np.sum(X_test[item].isnull()) / len(X_test)
+    if tmp < 0.95:
+        col_test.append(item)
 
-X_train.to_csv("./dataset/x_train_tmp.csv", encoding='utf-8')
 
-# X_test = pd.read_csv('./dataset/test.csv', encoding='utf-8',
-#                      low_memory=False, header='infer', index_col='vid')
+# 选择训练集和测试集的交集
+col = [item for item in col_train if item in col_test]
+print('len(col):', len(col))
+X_train, X_test = X_train[col], X_test[col]
+print(X_train.shape, X_test.shape)
 
-# print(X_train.shape, X_test.shape)
-# print("Start filter features!!!")
-# # 筛选缺失率小于0.1的特征
-# col_train, col_test = [], []
-# for item in X_train.columns[:]:
-#     tmp = np.sum(X_train[item].isnull()) / len(X_train)
-#     if tmp < 0.1:
-#         col_train.append(item)
-# for item in X_test.columns[:]:
-#     tmp = np.sum(X_test[item].isnull()) / len(X_test)
-#     if tmp < 0.1:
-#         col_test.append(item)
-# # 选择训练集和测试集的交集
-# col = [item for item in col_train if item in col_test]
-# print('len(col):', len(col))
-# X_train, X_test = X_train[col], X_test[col]
-# print(X_train.shape, X_test.shape)
+X_train, X_test = X_train.fillna(0), X_test.fillna(0)
+print(X_train.info(), X_test.info())
 
-# X_train, X_test = X_train.fillna(0), X_test.fillna(0)
-
-# X_train = X_train.convert_objects(convert_numeric=True)
-# X_test = X_test.convert_objects(convert_numeric=True)
-
-# X_train, X_test = X_train.fillna(0), X_test.fillna(0)
-
-# X_train = X_train.convert_objects(convert_numeric=True)
-# X_test = X_test.convert_objects(convert_numeric=True)
-
-# print(X_train.info(), X_test.info())
-
-# X_train.to_csv("./dataset/x_train_tmp.csv", encoding='utf-8')
-# X_test.to_csv("./dataset/x_test_tmp.csv", encoding='utf-8')
+X_train.to_csv("./dataset/x_train.csv", encoding='utf-8')
+X_test.to_csv("./dataset/x_test.csv", encoding='utf-8')
--- KNN.py
+++ KNN.py
@@ -2,78 +2,36 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_log_error
 
-#对训练集和测试集的y按照vid排序,和X一一对应
-y_train = pd.read_csv('./dataset/round1_train.csv', encoding='utf-8', low_memory=False)
-y_test = pd.read_csv('./dataset/round1_test.csv', encoding='utf-8', low_memory=False)
-y_train = y_train.sort_values('vid')
-y_test = y_test.sort_values('vid')
 
-y_train.to_csv("./dataset/y_train.csv", encoding='utf-8',index=False)
-y_test.to_csv("./dataset/y_test.csv", encoding='utf-8',index=False)
-print("Start split train and test dataset!!!")
 # 读取训练集和测试集
-X_train = pd.read_csv('./dataset/train.csv', encoding='utf-8', low_memory=False,header='infer')
-X_test = pd.read_csv('./dataset/test.csv', encoding='utf-8', low_memory=False,header='infer')
-y_train = pd.read_csv('./dataset/y_train.csv', encoding='utf-8', low_memory=False,header='infer')
-y_test = pd.read_csv('./dataset/y_test.csv', encoding='utf-8', low_memory=False,header='infer')
+X_train = pd.read_csv('./dataset/x_train.csv', encoding='utf-8', low_memory=False,header='infer',index_col='vid')
+X_test = pd.read_csv('./dataset/x_test.csv', encoding='utf-8', low_memory=False,header='infer',index_col='vid')
+y_train = pd.read_csv('./dataset/y_train.csv', encoding='utf-8', low_memory=False,header='infer',index_col='vid')
+y_test = pd.read_csv('./dataset/y_test.csv', encoding='utf-8', low_memory=False,header='infer',index_col='vid')
+# print(X_train.info(), X_test.info())
+# print(y_train.info(), y_test.info())
+# print(np.isnan(X_train).any())
+# print(np.isnan(y_train).any())
 
-X_train,X_test=X_train.set_index('vid'),X_test.set_index('vid')
-# y_train,y_test=y_train.set_index('vid'),y_test.set_index('vid')
-y_train=y_train.convert_objects(convert_numeric=True)
-y_test=y_test.convert_objects(convert_numeric=True)
-print(y_train.info(),y_test.info())
-
-print(X_train.shape, X_test.shape)
-
-# print(y_train.info(),y_test.info())
-print("Start filter features!!!")
-# 筛选缺失率小于0.1
-col_train, col_test = [], []
-for item in X_train.columns[:]:
-    tmp = np.sum(X_train[item].isnull()) / len(X_train)
-    if tmp < 0.2:
-        col_train.append(item)
-for item in X_test.columns[:]:
-    tmp = np.sum(X_test[item].isnull()) / len(X_test)
-    if tmp < 0.2:
-        col_test.append(item)
-# 选择训练集和测试集的交集
-col = [item for item in col_train if item in col_test]
-print('len(col):', len(col))
-X_train, X_test = X_train[col], X_test[col]
-print(X_train.shape, X_test.shape)
-
-X_train,X_test=X_train.fillna(0),X_test.fillna(0)
-
-X_train=X_train.convert_objects(convert_numeric=True)
-X_test=X_test.convert_objects(convert_numeric=True)
-
-X_train,X_test=X_train.fillna(0),X_test.fillna(0)
-y_train,y_test=y_train.fillna(0),y_test.fillna(0)
-
-X_train=X_train.convert_objects(convert_numeric=True)
-X_test=X_test.convert_objects(convert_numeric=True)
-
-print(X_train.info(),X_test.info())
-
-# X_train.to_csv("./dataset/tmp.csv", encoding='utf-8', index=False)
 print("Start Traing!!!!!!!!!!!!!!!!!!!!!")
 
 y_train_LB, y_train_HB, y_train_TRI, y_train_HDL, y_train_LDL \
     = y_train['LB'], y_train['HB'], y_train['TRI'], y_train['HDL'], y_train['LDL']
 
-clf_LB=KNeighborsRegressor(n_neighbors=15,weights='distance',n_jobs=1)
-clf_HB=KNeighborsRegressor(n_neighbors=15,weights='uniform',n_jobs=1)
-clf_TRI=KNeighborsRegressor(n_neighbors=15,weights='uniform',n_jobs=1)
-clf_HDL=KNeighborsRegressor(n_neighbors=15,weights='uniform',n_jobs=1)
-clf_LDL=KNeighborsRegressor(n_neighbors=15,weights='uniform',n_jobs=1)
+clf_LB=KNeighborsRegressor(n_neighbors=80,weights='uniform',n_jobs=-1)
+clf_HB=KNeighborsRegressor(n_neighbors=30,weights='uniform',n_jobs=-1)
+clf_TRI=KNeighborsRegressor(n_neighbors=95,weights='uniform',n_jobs=-1)
+clf_HDL=KNeighborsRegressor(n_neighbors=35,weights='uniform',n_jobs=-1)
+clf_LDL=KNeighborsRegressor(n_neighbors=35,weights='uniform',n_jobs=-1)
 
 clf_LB.fit(X_train,y_train_LB)
 clf_HB.fit(X_train,y_train_HB)
 clf_TRI.fit(X_train,y_train_TRI)
 clf_HDL.fit(X_train,y_train_HDL)
 clf_LDL.fit(X_train,y_train_LDL)
+
 
 y_pred_LB,y_pred_HB,y_pred_TRI,y_pred_HDL,y_pred_LDL=\
     clf_LB.predict(X_test),clf_HB.predict(X_test),clf_TRI.predict(X_test),\
@@ -82,46 +40,58 @@ y_pred_LB,y_pred_HB,y_pred_TRI,y_pred_HDL,y_pred_LDL=\
     pd.DataFrame(y_pred_LB),pd.DataFrame(y_pred_HB),pd.DataFrame(y_pred_TRI),\
     pd.DataFrame(y_pred_HDL),pd.DataFrame(y_pred_LDL)
 
-# y_train_LB.to_csv("./dataset/tmp1.csv", encoding='utf-8')
-# y_train_HB.to_csv("./dataset/tmp2.csv", encoding='utf-8')
-# y_train_TRI.to_csv("./dataset/tmp3.csv", encoding='utf-8')
-# y_train_HDL.to_csv("./dataset/tmp4.csv", encoding='utf-8')
-# y_train_LDL.to_csv("./dataset/tmp5.csv", encoding='utf-8')
+print(np.sum(y_train_LDL.isnull()))
+
+
+y_LB,y_HB,y_TRI,y_HDL,y_LDL=\
+    clf_LB.predict(X_train),clf_HB.predict(X_train),clf_TRI.predict(X_train),\
+    clf_HDL.predict(X_train),clf_LDL.predict(X_train)
+
+print(y_LDL)
+
+print("MSE_LB",mean_squared_log_error(y_train_LB,y_LB))
+print("MSE_HB",mean_squared_log_error(y_train_HB,y_HB))
+print("MSE_TRI",mean_squared_log_error(y_train_TRI,y_TRI))
+print("MSE_HDL",mean_squared_log_error(y_train_HDL,y_HDL))
+print("MSE_LDL",mean_squared_log_error(y_train_LDL,y_LDL))
+
+
+
 result=pd.concat([y_pred_LB,y_pred_HB,y_pred_TRI,y_pred_HDL,y_pred_LDL],axis=1)
-result.to_csv("./dataset/PredResultKNN0426.csv", encoding='utf-8',index=False)
+result.to_csv("./dataset/PredResultKNN0502.csv", encoding='utf-8',index=False)
 
 # clf=KNeighborsRegressor()
 # clf.fit(X_train,y_train_LB)
-# k_range=list(np.arange(15,20))
+# k_range=list(np.arange(5,100,5))
 # weights_option=['uniform','distance']
 # par_grid=dict(n_neighbors=k_range,weights=weights_option)
 
 
-# grid_knn_1=GridSearchCV(clf,par_grid,cv=5,scoring='r2',n_jobs=-1,verbose=1)
+# grid_knn_1=GridSearchCV(clf,par_grid,cv=10,scoring='neg_mean_squared_log_error',n_jobs=-1,verbose=1)
 # grid_knn_1.fit(X_train,y_train_LB)
 # print('LB.best_estimator_',grid_knn_1.best_estimator_)
 # print('LB.best_score_',grid_knn_1.best_score_)
 # print('LB.best_params_',grid_knn_1.best_params_)
 
-# grid_knn_2=GridSearchCV(clf,par_grid,cv=5,scoring='r2',n_jobs=-1,verbose=1)
+# grid_knn_2=GridSearchCV(clf,par_grid,cv=10,scoring='neg_mean_squared_log_error',n_jobs=-1,verbose=1)
 # grid_knn_2.fit(X_train,y_train_HB)
 # print('HB.best_estimator_',grid_knn_2.best_estimator_)
 # print('HB.best_score_',grid_knn_2.best_score_)
 # print('HB.best_params_',grid_knn_2.best_params_)
 
-# grid_knn_3=GridSearchCV(clf,par_grid,cv=5,scoring='r2',n_jobs=-1,verbose=1)
+# grid_knn_3=GridSearchCV(clf,par_grid,cv=10,scoring='neg_mean_squared_log_error',n_jobs=-1,verbose=1)
 # grid_knn_3.fit(X_train,y_train_TRI)
 # print('TRI.best_estimator_',grid_knn_3.best_estimator_)
 # print('TRI.best_score_',grid_knn_3.best_score_)
 # print('TRI.best_params_',grid_knn_3.best_params_)
 
-# grid_knn_4=GridSearchCV(clf,par_grid,cv=5,scoring='r2',n_jobs=-1,verbose=1)
+# grid_knn_4=GridSearchCV(clf,par_grid,cv=10,scoring='neg_mean_squared_log_error',n_jobs=-1,verbose=1)
 # grid_knn_4.fit(X_train,y_train_HDL)
 # print('HDL.best_estimator_',grid_knn_4.best_estimator_)
 # print('HDL.best_score_',grid_knn_4.best_score_)
 # print('HDL.best_params_',grid_knn_4.best_params_)
 
-# grid_knn_5=GridSearchCV(clf,par_grid,cv=5,scoring='r2',n_jobs=-1,verbose=1)
+# grid_knn_5=GridSearchCV(clf,par_grid,cv=10 ,scoring='neg_mean_squared_log_error',n_jobs=-1,verbose=1)
 # grid_knn_5.fit(X_train,y_train_LDL)
 # print('LDL.best_estimator_',grid_knn_5.best_estimator_)
 # print('LDL.best_score_',grid_knn_5.best_score_)
